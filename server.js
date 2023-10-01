aaaf549e1b60e39f4cfafc029c1c272e70ec92dd
Melissa <melissakalish2@gmail.com>
2023-10-01 15:30:44 -0500

    added code to file

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const expressHandlebars = require('express-handlebars');
+const helpers = require("./utils/helpers");
+const routes = require('./routes');
+const path = require('path');
+const session = require('express-session')
+const sequelize = require('./connection/connection');
+
+const db = require('./connection/connection');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const PORT = process.env.PORT || 3000;
+
+const expHbs = expressHandlebars.create({
+  helpers,
+});
+
+const app = express();
+
+// app.set('view engine', 'handlebars');
+
+app.engine('handlebars', expHbs.engine);
+app.set('view engine', 'handlebars');
+
+// creating user session
+const userSession = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 60 * 60 * 2000,
+        // httpOnly: true,
+        // secure: false,
+        // sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    })
+};
+
+app.use(session(userSession))
+
+// app.use for routes needs to be after session
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}`)
+    })
+});
