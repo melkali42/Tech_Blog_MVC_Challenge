67a25033b7f00b573c11a162f63f7c24f4983e98
Melissa <melissakalish2@gmail.com>
2023-10-03 19:15:40 -0500

    changed code for date code

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,16 +1,9 @@
 module.exports = {
-    format_date: date => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
-    }
-};
-
-
-// Path: utils/auth.js
-const withAuth = (req, res, next) => {
-    if (!req.session.user_id) {
-        res.redirect('/login');
-    } else {
-        next();
-    }
-};
-module.exports = withAuth;
+    format_date: (date) => {
+      // Format date as MM/DD/YYYY
+      return date.toLocaleDateString();
+    },
+    
+  };
+  
+  
