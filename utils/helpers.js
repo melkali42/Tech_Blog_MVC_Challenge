5227129063b218daaf519413b493a6c0f74b9a80
Melissa <melissakalish2@gmail.com>
2023-10-03 19:08:15 -0500

    error fix

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,3 +3,14 @@ module.exports = {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     }
 };
+
+
+// Path: utils/auth.js
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+module.exports = withAuth;
