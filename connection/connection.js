08f7589b648293a9c4975e2f4848bed5d1f99e6b
Melissa <melissakalish2@gmail.com>
2023-10-03 19:02:32 -0500

    error fix

--- connection/connection.js
+++ connection/connection.js
@@ -13,7 +13,7 @@ if (process.env.CLEARDB_DATABASE_URL) {
         {
             host: 'localhost',
             dialect: 'mysql',
-            port: 3306,
+            // port: 3306,
             },
         
         );
